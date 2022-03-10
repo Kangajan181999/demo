01e65fcc452f5c430524766a872b6893ef5c229d
kangajan181999 <kangajan.kuganathan@techventuras.com>
2022-03-10 12:51:47 +0530

    add_customer_service

---
+++ src/main/java/com/codex/demo/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.codex.demo.service;
+
+import com.codex.demo.dto.CustomerDTO;
+import com.codex.demo.model.Customer;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CustomerService {
+
+    ResponseEntity<Map<String,Boolean>> save(CustomerDTO customerDto);
+
+    List<Customer> getAllCustomer();
+}
